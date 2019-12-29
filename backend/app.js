5143c31ebb7507d2c48173bee6708f0d0f0eab7c
atmanand, sah <atmanand17@gmail.com>
2019-12-29 23:11:34 +0530

    initial

---
+++ backend/app.js
@@ -0,0 +1,133 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var Quiz = require('./routes/quiz');
+var Question = require('./routes/question');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use((req,res,next)=>{
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Header","*");
+  res.header("'Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+  next();
+})
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect('mongodb://localhost/quiz',{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=>console.log("database connected"));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+app.get('/',function(req,res){
+  res.send('hellow')
+});
+
+
+
+//get quiz method
+app.get('/api/quiz',function(req,res){
+  //console.log("requested")
+  Quiz.getQuiz(function(err,quiz){
+    if(err){
+      throw err;
+    }
+    console.log(quiz)
+    res.json(quiz);
+  });
+
+});
+
+//post quiz method
+app.post('/api/quiz',function(req,res){
+  console.log("requested")
+  //console.log(req);
+  var quiz =req.body;
+  Quiz.addQuiz(quiz,function(err,quiz){
+    if(err){
+      throw err;
+    }
+    res.json(quiz);
+  });
+ 
+
+});
+
+//get question method
+app.get('/api/question',function(req,res){
+  //console.log("requested")
+  Question.getQuestion(function(err,question){
+    if(err){
+      throw err;
+    }
+    console.log(question)
+    res.json(question);
+  });
+
+});
+
+app.get('/api/quiz_question',function(req,res){
+  var quiz_id = req.query.qid;
+  var queryObj = {
+    Quiz:quiz_id
+  }
+  Question.getQuizQuestion(queryObj,function(err,questions){
+    if(err)
+      throw err;
+    console.log(questions)
+    res.json(questions);
+  })
+})
+
+
+//post question method
+app.post('/api/question',function(req,res){
+  console.log('requested');
+  var q = req.body;
+  console.log("received body",q);
+  Question.addQuestion(q,function(err,question){
+    if(err){
+      throw err;
+    }
+    console.log(question);
+    res.json(question);
+  });
+});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+app.listen(8081);
+console.log('Running on port 8081...');
---
+++ frontend/front_app.js
@@ -0,0 +1,138 @@
+var quizListContainer;
+var quizQuestionListContainer;
+var headingTitle;
+var questionItemTemplate;
+
+function getQuestionListFromApiWithId(quiz_id){
+    var req_url = "http://localhost:8081/api/quiz_question?qid="+quiz_id;
+
+    return fetch(req_url)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+        return data;
+    })
+    .catch(err=>console.error(err));
+}
+
+function getQuizListFromApi(){
+
+    return fetch("http://localhost:8081/api/quiz")
+    .then((res)=>res.json())
+    .then((data)=>{
+        return data;
+    })
+    .catch((err)=>console.log(err))
+}
+
+function switchUI(type,data){
+    if(type === "questions"){
+        //remove quiz_container ul
+        quizListContainer.innerHTML="";
+        headingTitle.innerText = "Please attempt the following questions: ";
+        renderQuestionList(data)
+    }
+    else if(type === "quiz"){
+        quizQuestionListContainer.innerHTML = "";
+        headingTitle.innerText = "Select a Quiz"
+        renderQuiz(data)
+    }
+}
+
+function renderQuiz(data){
+    //
+    console.log("mydata",data)
+    const list_parent = document.createElement('ul');
+    list_parent.setAttribute('id',"quizes")
+    list_parent.setAttribute('class',"list-group");
+
+    var list_items = []
+    for( var i = 0;i<data.length;i++){
+        var list_item = document.createElement('li');
+        list_item.setAttribute('class','list-group-item')
+        list_item.setAttribute('id',data[i]._id)
+        list_item.setAttribute('data-name',data[i].name)
+        list_item.innerText = data[i].name;
+        list_item.addEventListener('click',(e)=>{
+            var quiz_id = e.target.getAttribute('id');
+            console.log(quiz_id+ " was requested")
+            getQuestionListFromApiWithId(quiz_id)
+            .then((questions)=> switchUI("questions",questions))
+        })
+        list_parent.appendChild(list_item)
+    }
+    quizListContainer.appendChild(list_parent);
+}
+
+function renderQuestionList(questions_list){
+    //{name":"where is the capital of india","option":"delhi,punjab,goa,godda","correct_option":
+    var list_parent = document.createElement('ul');
+    list_parent.setAttribute('id','question_list')
+    list_parent.setAttribute('class','list-group')
+
+
+    for(var i=0;i<questions_list.length;i++){
+        var list_item = questionItemTemplate.cloneNode(true)
+        var childrenOfListItem = list_item.children;
+        //console.log(childrenOfListItem)
+        //change card-header
+        childrenOfListItem[0].innerText = "Question "+(i+1);
+        var childrenOfCardBody = childrenOfListItem[1].children;
+        //change card question
+        childrenOfCardBody[0].innerText = questions_list[i].name;
+        /*document.getElementById('question_list').appendChild(list_item2);
+
+        var childrenOfForm = childrenOfCardBody[1].children;
+        for(var i=0;i<childrenOfForm.length;i++){
+            var childrenOfOptions = childrenOfForm[i].children;
+
+        }*/
+        list_parent.appendChild(list_item);
+    }
+    quizQuestionListContainer.appendChild(list_parent)
+}
+
+function renderOption(option){
+    
+}
+
+
+window.onload = function(){
+
+    //set references to elements in dom
+    headingTitle = document.getElementById('heading');
+    quizListContainer = document.getElementById('quiz_container')
+    quizQuestionListContainer = document.getElementById('question_container')
+    questionItemTemplate = document.getElementsByClassName('question-template')[0];
+    questionItemTemplate = questionItemTemplate.parentElement.removeChild(questionItemTemplate);
+
+    getQuizListFromApi()
+    .then((quiz_list)=>{
+        switchUI("quiz",quiz_list);
+    })
+
+    //testing
+    //rendertempQuestions()
+
+}
+
+function rendertempQuestions(){
+
+    var list_item = document.getElementsByClassName('question-template')[0];
+    var list_item2 = list_item.cloneNode(true);
+
+    var childrenOfListItem = list_item2.children;
+    console.log(childrenOfListItem)
+    //change card-header
+    childrenOfListItem[0].innerText = "Question 2";
+    var childrenOfCardBody = childrenOfListItem[1].children;
+    //change card question
+    childrenOfCardBody[0].innerText = "Question text 2";
+    document.getElementById('question_list').appendChild(list_item2);
+    
+    var childrenOfForm = childrenOfCardBody[1].children;
+    for(var i=0;i<childrenOfForm.length;i++){
+        var childrenOfOptions = childrenOfForm[i].children;
+
+    }
+}
